c77e0091926f7498176363f67c6bfa726c453ca1
khalilakbari <khalilakbari1400@gmail.com>
2023-10-29 12:45:29 +0330

    create componenet FlexContent & coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Hero, Sales } from './componenets'
-import { heroapi, popularsales, toprateslaes } from './data/data'
+import { Hero, Sales, FlexContent } from './componenets'
+import { heroapi, popularsales, toprateslaes, highlight, sneaker } from './data/data'
 
 export default function App() {
   return (
@@ -8,7 +8,9 @@ export default function App() {
        <main className='flex flex-col gap-16 relative'>
           <Hero heroapi={heroapi}/>
           <Sales endpoint={popularsales} ifExists/>
+          <FlexContent endpoint={highlight} ifExists/>
           <Sales endpoint={toprateslaes}/>
+          <FlexContent endpoint={sneaker}/>
        </main>
     </>
   )
