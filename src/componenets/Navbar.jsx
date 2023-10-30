cc29fbe1b998c8599c47f173a517427aa549c8e3
khalilakbari <khalilakbari1400@gmail.com>
2023-10-30 12:57:24 +0330

    create file store and cartslice and coding

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -3,10 +3,19 @@ import { FiSearch } from 'react-icons/fi'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { LiaShoppingBagSolid } from "react-icons/lia";
 import logo from '../assets/imagees/logo.png'
+import { useDispatch } from 'react-redux';
+import { setOpenCart } from '../app/CartSlice';
 
 const Navbar = () => {
 
   const[navState, setNavState] = useState(false)
+  const dispatch = useDispatch()
+
+  const onCartToggle = () => {
+     dispatch(setOpenCart({
+      cartState: true
+     }))
+  }
   
   const onNavScroll = () => {
     if (window.scrollY > 30) {
@@ -44,7 +53,8 @@ const Navbar = () => {
                       <AiOutlineHeart className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`}/>
                   </li>
                   <li className='grid items-center'>
-                      <button type='button' className='relative border-none outline-none active:scale-110 transition-all duration-300'>
+                      <button type='button' onClick={onCartToggle}
+                       className='relative border-none outline-none active:scale-110 transition-all duration-300'>
                          <LiaShoppingBagSolid className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`}/>
                          <div className={`absolute top-4 right-0 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full
                            flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 shadow
